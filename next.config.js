b54b2c6038b22df7907f1ea5f936d467c790fd69
celsojuniorladislau <celsojuniorladislau@gmail.com>
2025-05-02 12:05:13 -0300

    Envio de deploy.yml para o github actions

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  distDir: 'out'
-}
+    distDir: 'out',
+    basePath: process.env.NODE_ENV === 'production' ? '/SistemaDeImpressaoDeEtiquetas' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/SistemaDeImpressaoDeEtiquetas/' : '',
+    images: {
+      unoptimized: true,
+    }
+  }
 
 module.exports = nextConfig
